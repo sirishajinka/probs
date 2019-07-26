96c01ebeae0a7b7792d89ec4a6f36a49fcbe0367
sirishajinka <53328878+sirishajinka@users.noreply.github.com>
2019-07-26 13:40:25 +0530

    Create positiveornegative.c

---
+++ positiveornegative.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{int n;
+scanf("%d\n",&n);
+if(n>0)
+{printf("Positive");
+}
+else if(n<0)
+{printf("Negative");
+}
+else
+{printf("Zero");
+}
+return 0;
+}
